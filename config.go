1423562488ba498e5e1c312ae7730bdea6899dca
krishnan2098 <krishnan2098@gmail.com>
2019-06-10 23:55:17 +0530

    Update struct to fetch right datatype of parameter

--- config.go
+++ config.go
@@ -1,23 +1,23 @@
 package config
 
 type NRconfig struct {
-	ENV                     string `json:"ENV"`
-	SCENARIO                string `json:"SCENARIO"`
-	CONFIG                  string `json:"CONFIG"`
-	LAYOUTTYPE              int    `json:"LAYOUTTYPE"`
-	ISD                     int    `json:"ISD"`
-	FcGHz                   int    `json:"FcGHz"`
-	BandwidthMHz            int    `json:"BandwidthMHz"`
-	SCSKHz                  int    `json:"SCSKHz"`
-	Duplexity               string `json:"Duplexity"`
-	AntennaScheme           string `json:"AntennaScheme"`
-	ReliabiltyAntennaScheme string `json:"ReliabiltyAntennaScheme"`
-	BSAntennaConfig         []int  `json:"BSAntennaConfig"`
-	UEAntennaConfig         string `json:"UEAntennaConfig"`
-	FrameStructure          string `json:"FrameStructure"`
-	ChannelModel            string `json:"ChannelModel"`
-	NumTRxP                 string `json:"NumTRxP"`
-	MobilityClass           string `json:"MobilityClass"`
+	ENV                     string  `json:"ENV"`
+	SCENARIO                string  `json:"SCENARIO"`
+	CONFIG                  string  `json:"CONFIG"`
+	LAYOUTTYPE              int     `json:"LAYOUTTYPE"`
+	ISD                     int     `json:"ISD"`
+	FcGHz                   float64 `json:"FcGHz"`
+	BandwidthMHz            int     `json:"BandwidthMHz"`
+	SCSKHz                  int     `json:"SCSKHz"`
+	Duplexity               string  `json:"Duplexity"`
+	AntennaScheme           string  `json:"AntennaScheme"`
+	ReliabiltyAntennaScheme string  `json:"ReliabiltyAntennaScheme"`
+	BSAntennaConfig         []int   `json:"BSAntennaConfig"`
+	UEAntennaConfig         string  `json:"UEAntennaConfig"`
+	FrameStructure          string  `json:"FrameStructure"`
+	ChannelModel            string  `json:"ChannelModel"`
+	NumTRxP                 string  `json:"NumTRxP"`
+	MobilityClass           int     `json:"MobilityClass"`
 }
 
 type ITUConfig struct {
