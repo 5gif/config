9b718053579ee0d16058c51ae489194ee98e68e8
krishnan2098 <krishnan2098@gmail.com>
2019-06-10 23:44:32 +0530

    Add all config structs to config.go

--- config.go
+++ config.go
@@ -1,5 +1,25 @@
 package config
 
+type NRconfig struct {
+	ENV                     string `json:"ENV"`
+	SCENARIO                string `json:"SCENARIO"`
+	CONFIG                  string `json:"CONFIG"`
+	LAYOUTTYPE              int    `json:"LAYOUTTYPE"`
+	ISD                     int    `json:"ISD"`
+	FcGHz                   int    `json:"FcGHz"`
+	BandwidthMHz            int    `json:"BandwidthMHz"`
+	SCSKHz                  int    `json:"SCSKHz"`
+	Duplexity               string `json:"Duplexity"`
+	AntennaScheme           string `json:"AntennaScheme"`
+	ReliabiltyAntennaScheme string `json:"ReliabiltyAntennaScheme"`
+	BSAntennaConfig         []int  `json:"BSAntennaConfig"`
+	UEAntennaConfig         string `json:"UEAntennaConfig"`
+	FrameStructure          string `json:"FrameStructure"`
+	ChannelModel            string `json:"ChannelModel"`
+	NumTRxP                 string `json:"NumTRxP"`
+	MobilityClass           string `json:"MobilityClass"`
+}
+
 type ITUConfig struct {
 	ENV                 string  `json:"ENV"`
 	SCENARIO            string  `json:"SCENARIO"`
@@ -11,15 +31,15 @@ type ITUConfig struct {
 	Duplexity           string  `json:"Duplexity"`
 	BSHeight            int     `json:"BSHeight"`
 	UEHeightout         float64 `json:"UEHeightout"`
-	UEHeightin          int     `json:"UEHeightin"`
+	UEHeightin          float64 `json:"UEHeightin"`
 	BSTxPowerDbm        int     `json:"BSTxPowerDbm"`
 	BandwidthMHz        int     `json:"BandwidthMHz"`
 	UETxDbm             int     `json:"UETxDbm"`
-	BuildingTypeLoss    int     `json:"BuildingTypeLoss"`
+	BuildingTypeLoss    float64 `json:"BuildingTypeLoss"`
 	ISD                 int     `json:"ISD"`
 	NumBSelements       int     `json:"NumBSelements"`
 	NumUEelements       int     `json:"NumUEelements"`
-	INDOORRatio         int     `json:"INDOORRatio"`
+	INDOORRatio         float64 `json:"INDOORRatio"`
 	IndoorSpeed         int     `json:"IndoorSpeed"`
 	Outdoorspeed        int     `json:"Outdoorspeed"`
 	BSNoiseFigureDb     int     `json:"BSNoiseFigureDb"`
