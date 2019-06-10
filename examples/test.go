047b5d61d67ffc25c60842fcf6596ef204e421ed
krishnan2098 <krishnan2098@gmail.com>
2019-06-11 00:11:31 +0530

    updated path to config file

--- examples/test.go
+++ examples/test.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	var confg config.NRconfig
-	var path2file = "/home/krishnan2098/goapps/src/github.com/5gif/config/3GPP_Ru_configA.json" //Add absolute path to the json file
+	var path2file = "/home/krishnan2098/goapps/src/github.com/5gif/config/json/3GPP_Ru_configA.json" //Add absolute path to the json file
 	vlib.LoadStructure(path2file, &confg)
 	fmt.Println(confg.FcGHz)
 }
