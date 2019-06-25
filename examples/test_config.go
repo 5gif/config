51ac1f6942e03357af05aa284fcf0c628ee184e1
wiless <wiless.bytes@gmail.com>
2019-06-26 01:11:22 +0530

    minor print changes

--- examples/test_config.go
+++ examples/test_config.go
@@ -13,10 +13,10 @@ func main() {
 	var nrcfg config.NRconfig                // loads the 3GPP related parameters for the RURAL Evaluation Config. A (of ITU-R/WP5D)
 	path1 := "../json/3GPP_RMa_configA.json" // Add absolute path to the json file
 	vlib.LoadStructure(path1, &nrcfg)
-	fmt.Println(nrcfg.FcGHz)
+	fmt.Printf("\nNR Config = %#v", nrcfg)
 
 	var ituconfig config.ITUconfig          // loads the 3GPP related parameters for the RURAL Evaluation Config. A (of ITU-R/WP5D)
 	path2 := "../json/ITU_RMa_configA.json" // Add absolute path to the json file
 	vlib.LoadStructure(path2, &ituconfig)
-	fmt.Println(ituconfig.FcGHz)
+	fmt.Printf("\n\nITU-R  Config = %#v", ituconfig)
 }
