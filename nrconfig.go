f13d43540e398d5fc1477765cfcfeb4ad533b808
wiless <wiless@users.noreply.github.com>
2019-06-27 21:12:07 +0530

    Create nrconfig.go

---
+++ nrconfig.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"os"
+	"path/filepath"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/spf13/viper"
+	"github.com/wiless/vlib"
+)
+
+// NRconfig structure
+type NRconfig struct {
+	ENV                     string  `json:"ENV"`
+	SCENARIO                string  `json:"SCENARIO"`
+	CONFIG                  string  `json:"CONFIG"`
+	LAYOUTTYPE              int     `json:"LAYOUTTYPE"`
+	ISD                     int     `json:"ISD"`
+	FcGHz                   float64 `json:"FcGHz"`
+	BandwidthMHz            float64 `json:"BandwidthMHz"`
+	SCSKHz                  int     `json:"SCSKHz"`
+	Duplexity               string  `json:"Duplexity"`
+	AntennaScheme           string  `json:"AntennaScheme"`
+	ReliabiltyAntennaScheme string  `json:"ReliabiltyAntennaScheme"`
+	BSAntennaConfig         []int   `json:"BSAntennaConfig"`
+	UEAntennaConfig         string  `json:"UEAntennaConfig"`
+	FrameStructure          string  `json:"FrameStructure"`
+	ChannelModel            string  `json:"ChannelModel"`
+	NumTRxP                 string  `json:"NumTRxP"`
+	MobilityClass           int     `json:"MobilityClass"`
+}
