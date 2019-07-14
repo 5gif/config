f802d2e1da0e5f0b63556725cf57b6814a8f52df
krishnan2098 <krishnan2098@gmail.com>
2019-07-14 13:09:53 +0530

    updated test_config.go to handle the new function params

--- examples/test_config.go
+++ examples/test_config.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/5gif/config"
 	"github.com/wiless/vlib"
 )
 
 func main() {
-
-	config.SetDir("../json", "results")
+	pwd, _ := os.Getwd()
+	cfgDir := pwd + "/" + "../json"
+	fmt.Println(cfgDir)
+	config.SetDir(cfgDir, "results")
 
 	fmt.Println("\n\n=========== Use method 1 ===========")
 	var nrcfg config.NRconfig                // loads the 3GPP related parameters for the RURAL Evaluation Config. A (of ITU-R/WP5D)
@@ -29,17 +32,20 @@ func main() {
 	icfg.Save()
 
 	fmt.Println("\n\n=========== Use method 3 ===========")
-	icfg2 := config.ReadITUConfig("ITU_RMa_configA.json", "../json")
+	icfg2, err2 := config.ReadITUConfig("ITU_RMa_configA.json")
+	_ = err2
 	fmt.Printf("\n\nITU-R Method 3 Config = %#v\n", icfg2)
 	icfg2.Save()
 
 	fmt.Println("\n\n=========== Use method 3 ===========")
-	icfg3 := config.ReadSIMConfig("SIM_RMa_configA.json", "../json")
+	icfg3, err3 := config.ReadSIMConfig("SIM_RMa_configA.json")
+	_ = err3
 	fmt.Printf("\n\nSIM-R Method 3 Config = %#v\n", icfg3)
 	icfg3.Save()
 
 	fmt.Println("\n\n=========== Use method 3 ===========")
-	icfg4 := config.ReadNRConfig("3GPP_UMa_configA.json", "../json")
+	icfg4, err4 := config.ReadNRConfig("3GPP_UMa_configA.json")
+	_ = err4
 	fmt.Printf("\n\n3GPP-U Method 3 Config = %#v\n", icfg4)
 	icfg4.Save()
 
