81e503e897aba93bc13b40bf79f78f7c10e0239a
krishnan2098 <krishnan2098@gmail.com>
2019-08-04 15:38:47 +0530

    Updated a new method to load structures using setup() function

--- examples/test_config.go
+++ examples/test_config.go
@@ -18,35 +18,44 @@ func main() {
 	var nrcfg config.NRconfig                // loads the 3GPP related parameters for the RURAL Evaluation Config. A (of ITU-R/WP5D)
 	path1 := "../json/3GPP_RMa_configA.json" // Add absolute path to the json file
 	vlib.LoadStructure(path1, &nrcfg)
-	fmt.Printf("\nNR Config = %#v", nrcfg)
-
-	// var ituconfig config.ITUconfig          // loads the 3GPP related parameters for the RURAL Evaluation Config. A (of ITU-R/WP5D)
-	// path2 := "../json/ITU_RMa_configA.json" // Add absolute path to the json file
-	// vlib.LoadStructure(path2, &ituconfig)
-	// fmt.Printf("\n\nITU-R Config = %#v", ituconfig)
+	config.PrintStructsPretty(nrcfg)
 
 	fmt.Println("\n\n=========== Use method 2 ===========")
 	var icfg config.ITUconfig
 	icfg.Read("ITU_RMa_configB.json")
-	fmt.Printf("\n\nITU-R Method 2 Config = %#v\n", icfg)
+	config.PrintStructsPretty(icfg)
 	icfg.Save()
 
 	fmt.Println("\n\n=========== Use method 3 ===========")
 	icfg2, err2 := config.ReadITUConfig("ITU_RMa_configA.json")
 	_ = err2
-	fmt.Printf("\n\nITU-R Method 3 Config = %#v\n", icfg2)
+	config.PrintStructsPretty(icfg2)
 	icfg2.Save()
 
-	fmt.Println("\n\n=========== Use method 3 ===========")
+	fmt.Println("\n\n=========== Use method 4 ===========")
 	icfg3, err3 := config.ReadSIMConfig("SIM_RMa_configA.json")
 	_ = err3
-	fmt.Printf("\n\nSIM-R Method 3 Config = %#v\n", icfg3)
+	config.PrintStructsPretty(icfg3)
 	icfg3.Save()
 
-	fmt.Println("\n\n=========== Use method 3 ===========")
+	fmt.Println("\n\n=========== Use method 5 ===========")
 	icfg4, err4 := config.ReadNRConfig("3GPP_UMa_configA.json")
 	_ = err4
-	fmt.Printf("\n\n3GPP-U Method 3 Config = %#v\n", icfg4)
+	config.PrintStructsPretty(icfg4)
 	icfg4.Save()
 
+	fmt.Println("\n\n=========== Use method 6 ===========")
+	// The flow of main() to read calls a setup function using an input json file
+	var i config.ITUconfig
+	var n config.NRconfig
+	var s config.SIMconfig
+	var err error
+	var input = "default.json"
+
+	i, n, s, _, err = config.Setup(input)
+	if err == nil {
+		config.PrintStructsPretty(i)
+		config.PrintStructsPretty(n)
+		config.PrintStructsPretty(s)
+	}
 }
