d8f71e82326b971224edd17586852f2c1a992dd3
vishakhadhanwani <vishakhadhanwani@gmail.com>
2020-01-11 19:53:10 +0530

    Upadated configs after adding UE Antenna

--- nrconfig.go
+++ nrconfig.go
@@ -6,51 +6,77 @@ import (
 	"github.com/wiless/vlib"
 )
 
-// NRconfig structure
-// type NRconfig struct {
-// 	ENV                     string        `json:"ENV"`
-// 	SCENARIO                string        `json:"SCENARIO"`
-// 	CONFIG                  string        `json:"CONFIG"`
-// 	LAYOUTTYPE              int           `json:"LAYOUTTYPE"`
-// 	FcGHz                   float64       `json:"FcGHz"`
-// 	NumTRxP                 int           `json:"NumTRxP"`
-// 	AntennaScheme           string        `json:"AntennaScheme"`
-// 	BSAntennaConfig         []int         `json:"BSAntennaConfig"`
-// 	UEAntennaConfig         []interface{} `json:"UEAntennaConfig"`
-// 	SCSKHz                  int           `json:"SCSKHz"`
-// 	FrameStructure          string        `json:"FrameStructure"`
-// 	ChannelModel            string        `json:"ChannelModel"`
-// 	ReliabiltyAntennaScheme string        `json:"ReliabiltyAntennaScheme"`
-// 	MobilityClass           int           `json:"MobilityClass"`
-// 	fname                   string
-// }
-
 type NRconfig struct {
-	ENV                     string        `json:"ENV"`
-	SCENARIO                string        `json:"SCENARIO"`
-	CONFIG                  string        `json:"CONFIG"`
-	LAYOUTTYPE              int           `json:"LAYOUTTYPE"`
-	FcGHz                   float64       `json:"FcGHz"`
-	NumTRxP                 int           `json:"NumTRxP"`
-	AntennaScheme           string        `json:"AntennaScheme"`
-	BSAntennaConfig         []int         `json:"BSAntennaConfig"`
-	UEAntennaConfig         []interface{} `json:"UEAntennaConfig"`
-	SCSKHz                  int           `json:"SCSKHz"`
-	FrameStructure          string        `json:"FrameStructure"`
-	ChannelModel            string        `json:"ChannelModel"`
-	ReliabiltyAntennaScheme string        `json:"ReliabiltyAntennaScheme"`
-	MobilityClass           int           `json:"MobilityClass"`
-	GainDb                  float64       `json:"GainDb"`
-	VBeamWidth              float64       `json:"VBeamWidth"`
-	HBeamWidth              float64       `json:"HBeamWidth"`
-	SLAV                    float64       `json:"SLAV"`
-	ESpacingVFactor         float64       `json:"EspacingVfactor"`
-	ESpacingHFactor         float64       `json:"EspacingHfactor"`
-	ElectricalTilt          []float64     `json:"ElectricalTilt"`
-	Escan                   []float64     `json:"Escan"`
-	MechanicalTilt          float64       `json:"MechanicalTilt"`
-	fname                   string
-	Vtilt                   float64
+	AntennaScheme string `json:"AntennaScheme"`
+	BS            struct {
+		AntennaConfig  []int     `json:"AntennaConfig"`
+		ElectricalTilt []float64 `json:"ElectricalTilt"`
+		Escan          []float64 `json:"Escan"`
+		GainDb         float64   `json:"GainDb"`
+		HBeamWidth     float64   `json:"HBeamWidth"`
+		Omni           bool      `json:"Omni"`
+		SLAV           float64   `json:"SLAV"`
+		VBeamWidth     float64   `json:"VBeamWidth"`
+	} `json:"BS"`
+	CONFIG                  string  `json:"CONFIG"`
+	ChannelModel            string  `json:"ChannelModel"`
+	ENV                     string  `json:"ENV"`
+	EspacingHfactor         float64 `json:"EspacingHfactor"`
+	EspacingVfactor         float64 `json:"EspacingVfactor"`
+	FcGHz                   float64 `json:"FcGHz"`
+	FrameStructure          string  `json:"FrameStructure"`
+	LAYOUTTYPE              int     `json:"LAYOUTTYPE"`
+	MechanicalTilt          float64 `json:"MechanicalTilt"`
+	MobilityClass           int     `json:"MobilityClass"`
+	NumTRxP                 int     `json:"NumTRxP"`
+	ReliabiltyAntennaScheme string  `json:"ReliabiltyAntennaScheme"`
+	SCENARIO                string  `json:"SCENARIO"`
+	SCSKHz                  int     `json:"SCSKHz"`
+	UE                      struct {
+		AntennaConfig  []int     `json:"AntennaConfig"`
+		ElectricalTilt []float64 `json:"ElectricalTilt"`
+		Escan          []float64 `json:"Escan"`
+		GainDb         float64   `json:"GainDb"`
+		HBeamWidth     float64   `json:"HBeamWidth"`
+		Omni           bool      `json:"Omni"`
+		SLAV           float64   `json:"SLAV"`
+		VBeamWidth     float64   `json:"VBeamWidth"`
+	} `json:"UE"`
+	fname string `json:"fname"`
+	Vtilt float64
+
+	//        float64ENV                     string        `json:"ENV"`
+	// SCENARIO                string        `json:"SCENARIO"`
+	// CONFIG                  string        `json:"CONFIG"`
+	// LAYOUTTYPE              int           `json:"LAYOUTTYPE"`
+	// FcGHz                   float64       `json:"FcGHz"`
+	// NumTRxP                 int           `json:"NumTRxP"`
+	// AntennaScheme           string        `json:"AntennaScheme"`
+	// BSAntennaConfig         []int         `json:"BSAntennaConfig"`
+	// UEAntennaConfig         []int          `json:"UEAntennaConfig"`
+	// SCSKHz                  int           `json:"SCSKHz"`
+	// FrameStructure          string        `json:"FrameStructure"`
+	// ChannelModel            string        `json:"ChannelModel"`
+	// ReliabiltyAntennaScheme string        `json:"ReliabiltyAntennaScheme"`
+	// MobilityClass           int           `json:"MobilityClass"`
+	// GainDb                  float64       `json:"GainDb"`
+	// VBeamWidth              float64       `json:"VBeamWidth"`
+	// HBeamWidth              float64       `json:"HBeamWidth"`
+	// SLAV                    float64       `json:"SLAV"`
+	// UEGainDb                  float64       `json:"UEGainDb"`
+	// UEVBeamWidth              float64       `json:"UEVBeamWidth"`
+	// UEHBeamWidth              float64       `json:"UEHBeamWidth"`
+	// UESLAV                    float64       `json:"UESLAV"`
+	// ESpacingVFactor         float64       `json:"EspacingVfactor"`
+	// ESpacingHFactor         float64       `json:"EspacingHfactor"`
+	// ElectricalTilt          []float64     `json:"ElectricalTilt"`
+	// Escan                   []float64     `json:"Escan"`
+	// UEElectricalTilt        []float64     `json:"UEElectricalTilt"`
+	// UEEscan                 []float64     `json:"UEEscan"`
+	// MechanicalTilt          float64       `json:"MechanicalTilt"`
+	// Omni                    bool           `json:"Omni"`
+	// fname                   string
+	// Vtilt                   float64
 }
 
 // Save ...
@@ -87,3 +113,22 @@ func ReadNRConfig(configname string) (NRconfig, error) {
 	err := cfg.Read(configname)
 	return cfg, err
 }
+
+// NRconfig structure
+// type NRconfig struct {
+// 	ENV                     string        `json:"ENV"`
+// 	SCENARIO                string        `json:"SCENARIO"`
+// 	CONFIG                  string        `json:"CONFIG"`
+// 	LAYOUTTYPE              int           `json:"LAYOUTTYPE"`
+// 	FcGHz                   float64       `json:"FcGHz"`
+// 	NumTRxP                 int           `json:"NumTRxP"`
+// 	AntennaScheme           string        `json:"AntennaScheme"`
+// 	BSAntennaConfig         []int         `json:"BSAntennaConfig"`
+// 	UEAntennaConfig         []interface{} `json:"UEAntennaConfig"`
+// 	SCSKHz                  int           `json:"SCSKHz"`
+// 	FrameStructure          string        `json:"FrameStructure"`
+// 	ChannelModel            string        `json:"ChannelModel"`
+// 	ReliabiltyAntennaScheme string        `json:"ReliabiltyAntennaScheme"`
+// 	MobilityClass           int           `json:"MobilityClass"`
+// 	fname                   string
+// }
