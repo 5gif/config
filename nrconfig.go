2dad90f833c6f667fb3cb49e20f5e05bd4155394
vishakhadhanwani <vishakhadhanwani@gmail.com>
2020-03-09 20:55:41 +0530

    Added DefaultNRconfig()

--- nrconfig.go
+++ nrconfig.go
@@ -88,6 +88,53 @@ type NRconfig struct {
 	// Vtilt                   float64
 }
 
+func (i *NRconfig) DefaultNRconfig() {
+
+	i.ENV = "RMa"
+	i.SCENARIO = "eMBB"
+	i.CONFIG = "A"
+	i.LAYOUTTYPE = 0
+	i.FcGHz = 0.7
+	i.NumTRxP = 57
+	i.AntennaScheme = "32x4 MU-MIMO, Reciprocity based, 4T SRS"
+	i.BS.AntennaConfig = []int{8, 4, 2, 1, 1, 1, 2}
+	i.BS.SLAV = 30
+	i.BS.HBeamWidth = 65
+	i.BS.VBeamWidth = 65
+	i.BS.GainDb = 8
+	i.BS.ElectricalTilt = []float64{99.2}
+	i.BS.Escan = []float64{0.0}
+	i.BS.Omni = false
+	i.BS.PanelAz = []float64{0.0}
+	i.BS.PanelEl = []float64{90.0}
+	i.BS.EspacingHfactor = 0.5
+	i.BS.EspacingVfactor = 0.8
+	i.BS.MechanicalTilt = 90
+	i.BS.Polarization = []float64{45, -45}
+
+	i.UE.SLAV = 25
+	i.UE.AntennaConfig = []int{8, 4, 2, 1, 1, 1, 2}
+	i.UE.HBeamWidth = 90
+	i.UE.VBeamWidth = 90
+	i.UE.GainDb = 0
+	i.UE.ElectricalTilt = []float64{}
+	i.UE.Escan = []float64{}
+	i.UE.Omni = true
+	i.UE.PanelAz = []float64{}
+	i.UE.PanelEl = []float64{}
+	i.UE.EspacingHfactor = 0.5
+	i.UE.EspacingVfactor = 0.8
+	i.UE.MechanicalTilt = 90
+	i.UE.Polarization = []float64{0, 90}
+	i.SCSKHz = 30
+	i.FrameStructure = "DDDSU"
+	i.ChannelModel = "A"
+	i.ReliabiltyAntennaScheme = ""
+	i.MobilityClass = 0
+	i.fname = "3GPP_RMa_configA"
+
+}
+
 // Save ...
 func (i *NRconfig) Save() {
 
